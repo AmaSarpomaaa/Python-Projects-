c7b79f4ff0b0bb9de192549dd9e9ce969cf072ca
Christabel Osei <armahsarpomaa16@gmail.com>
2024-03-12 11:02:34 -0400

    added the game questions

---
+++ quiz_game.py
@@ -0,0 +1,41 @@
+print("Welcome to the computer quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay! Let's play :)")
+
+answer = input("What does CPU stands for? ")
+
+if answer.lower() == "central processing unit":
+    print("Correct!")
+else:
+    print("Incorect!")
+
+
+answer = input("What does CPU stands for? ")
+
+if answer.lower() == "central processing unit":
+    print("Correct!")
+else:
+    print("Incorect!")
+
+
+answer = input("What does CPU stands for? ")
+
+if answer.lower() == "central processing unit":
+    print("Correct!")
+else:
+    print("Incorect!")
+
+
+answer = input("What does CPU stands for? ")
+
+if answer.lower() == "central processing unit":
+    print("Correct!")
+else:
+    print("Incorect!")
+
+
